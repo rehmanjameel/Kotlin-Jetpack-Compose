548507157eddcf02f83b13598aad5fac7acea8da
rehmanjameel <abdulrehmancs17@gmail.com>
2023-10-13 17:43:18 +0500

    set image with painter and box

--- app/src/main/java/com/example/firstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeapp/MainActivity.kt
@@ -5,119 +5,90 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.firstcomposeapp.ui.theme.FirstComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            FirstComposeAppTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
+            val painter = painterResource(id = R.drawable.kermit)
+            val description = "kermit is hanging on <3 love pole"
+            val title = "kermit is hanging on love <3 pole"
 
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.Green)
-                    .fillMaxHeight(0.5f)
-//                    .width(600.dp)
-                    .border(5.dp, Color.Magenta)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Blue)
-                    .padding(5.dp)
-                    .border(10.dp, Color.Red)
-                    .padding(10.dp)
-//                horizontalAlignment = Alignment.CenterHorizontally,
-//                verticalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Text(text = "Hello", modifier = Modifier
-                    .border(5.dp, Color.Black)
-                    .padding(15.dp)
-                    .offset(10.dp, 20.dp)
-                    .border(10.dp, Color.Yellow)
-                    .padding(10.dp))
-                Spacer(modifier = Modifier.height(50.dp))
-                Text(text = "World!")
-
-
+            Box(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(16.dp)) {
+                ImageCard(
+                    painter = painter,
+                    contentDescription = description,
+                    title = title
+                )
             }
         }
+
     }
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Image(painter = painterResource(id = R.drawable.ic_launcher_foreground),
-//        contentDescription = null,
-//    modifier = Modifier.background(Color.Gray))
-    Column {
-        for (i in 1 .. 10) {
-            Icon(
-                imageVector = Icons.Default.Add,
-                contentDescription = null
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 5.dp
+        )
+    ) {
+
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.FillBounds
             )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    )
+                )
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+                contentAlignment = Alignment.BottomStart // align the text box at the bottom of image box
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
         }
-    }
-
-//    Box(
-//        modifier = Modifier.size(400.dp),
-//
-//    ) {
-//
-//        Text(
-//            text = "Hello $name!",
-//            color = Color.Blue,
-//            fontSize = 20.sp,
-//            modifier = Modifier.align(Alignment.BottomEnd)
-//        )
-//        Text(
-//            text = "Some Other text!",
-//            modifier = modifier,
-//            color = Color.Blue,
-//            fontSize = 20.sp
-//        )
-
-//    }
-}
-
-@Preview(showBackground = true, device = "spec:width=411dp,height=891dp")
-@Composable
-fun GreetingPreview() {
-    FirstComposeAppTheme {
-//        Greeting("ARJamil")
     }
 }
