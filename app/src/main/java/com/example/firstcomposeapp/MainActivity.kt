a09fe7e2b4e5ef565f2c808b12e7ea9f23b40304
rehmanjameel <abdulrehmancs17@gmail.com>
2023-10-13 18:41:22 +0500

    working on fonts

--- app/src/main/java/com/example/firstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeapp/MainActivity.kt
@@ -1,94 +1,50 @@
 package com.example.firstcomposeapp
 
+import android.graphics.fonts.FontFamily
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        val fontFamily: FontFamily(
+//            Font(R.font.lexend_regular, FontWeight.Thin),
+//            Font(R.font.lexend_black, FontWeight.Thin),
+//            Font(R.font.lexend_bold, FontWeight.Thin),
+//            Font(R.font.lexend_extrabold, FontWeight.Thin),
+//            Font(R.font.lexend_light, FontWeight.Thin),
+//            Font(R.font.lexend_medium, FontWeight.Thin),
+//            Font(R.font.lexend_semibold, FontWeight.Thin),
+//            Font(R.font.lexend_extralight, FontWeight.Thin)
+//        )
         setContent {
-            val painter = painterResource(id = R.drawable.kermit)
-            val description = "kermit is hanging on <3 love pole"
-            val title = "kermit is hanging on love <3 pole"
 
             Box(modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(16.dp)) {
-                ImageCard(
-                    painter = painter,
-                    contentDescription = description,
-                    title = title
+                .fillMaxSize()
+                .background(Color(0xFF10101010))) {
+                Text(text = "Jetpack compose",
+                    color = Color.White,
+                    fontSize = 30.sp
                 )
             }
+
         }
 
     }
 
-}
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 5.dp
-        )
-    ) {
-
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.FillBounds
-            )
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black
-                        ),
-                        startY = 300f
-                    )
-                )
-            )
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .padding(12.dp),
-                contentAlignment = Alignment.BottomStart // align the text box at the bottom of image box
-            ) {
-                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-            }
-        }
-    }
 }
